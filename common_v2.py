2159c6a9e9c5509da6e788889e83a051010a6f83
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-06-01 13:53:34 +0200

    outsourced functionality in preparation for similar http push example

---
+++ common_v2.py
@@ -0,0 +1,213 @@
+import time
+
+import requests
+from icecream import ic
+import json
+
+
+CONTEXT = {
+    "@vocab": "https://w3id.org/edc/v0.0.1/ns/",
+    "edc": "https://w3id.org/edc/v0.0.1/ns/",
+    "odrl": "http://www.w3.org/ns/odrl/2/",
+},
+
+EDC_PREFIX = "edc:"
+ODRL_PREFIX = "odrl:"
+
+
+def create_dataplane(transfer_url, public_api_url, connector_management_url, verbose=True):
+    provider_dp_instance_data = {
+        "edctype": "dataspaceconnector:dataplaneinstance",
+        "id": "http-pull-provider-dataplane",
+        "url": transfer_url,
+        "allowedSourceTypes": ["HttpData"],
+        "allowedDestTypes": ["HttpProxy", "HttpData"],
+        "properties": {
+            "publicApiUrl": public_api_url
+        }
+    }
+    response = requests.post(connector_management_url + "instances",
+                             headers={'Content-Type': 'application/json'},
+                             data=json.dumps(provider_dp_instance_data))
+    if verbose:
+        ic(response.status_code, response.text)
+
+
+def create_asset(asset_id, asset_name, asset_description, asset_version, asset_contenttype,
+                 data_name, data_base_url, data_type,
+                 connector_management_url, verbose=True):
+    asset_data = {
+        "@context": CONTEXT,
+        EDC_PREFIX + "asset": {
+            "@type": EDC_PREFIX + "Asset",
+            "@id": asset_id,
+            EDC_PREFIX + "properties": {
+                EDC_PREFIX + "name": asset_name,
+                EDC_PREFIX + "description": asset_description,
+                EDC_PREFIX + "version": asset_version,
+                EDC_PREFIX + "contenttype": asset_contenttype
+            }
+        },
+        EDC_PREFIX + "dataAddress": {
+            "@type": EDC_PREFIX + "DataAddress",
+            EDC_PREFIX + "properties": {
+                EDC_PREFIX + "name": data_name,
+                EDC_PREFIX + "baseUrl": data_base_url,
+                EDC_PREFIX + "type": data_type
+            }
+        }
+    }
+
+    response = requests.post(connector_management_url + "v2/assets",
+                             headers={'Content-Type': 'application/json'},
+                             data=json.dumps(asset_data))
+    if verbose:
+        ic(response.status_code, json.loads(response.text))
+    # extract asset id from response
+    return json.loads(response.text)["@id"]
+
+
+def create_policy(policy_id, target_asset_id, connector_management_url, verbose=True):  # TODO for now we always use the same permissions
+    policy_data = {
+        "@context": CONTEXT,
+        "@id": policy_id,
+        EDC_PREFIX + "policy": {
+            "@context": "http://www.w3.org/ns/odrl.jsonld",
+            ODRL_PREFIX + "permission": [
+                {
+                    ODRL_PREFIX + "target": target_asset_id,
+                    ODRL_PREFIX + "action": {
+                        ODRL_PREFIX + "type": "USE"
+                    },
+                    ODRL_PREFIX + "edctype": "dataspaceconnector:permission"
+                }
+            ],
+            "@type": ODRL_PREFIX + "Set"
+        }
+    }
+
+    response = requests.post(connector_management_url + "v2/policydefinitions",
+                             headers={'Content-Type': 'application/json'},
+                             data=json.dumps(policy_data))
+    if verbose:
+        ic(response.status_code, json.loads(response.text))
+    return json.loads(response.text)["@id"]
+
+
+def create_contract_definition(access_policy_id, contract_policy_id, connector_management_url, verbose=True):  # TODO for now we use no selector (i.e. all assets are selected)
+    contract_definition_data = {
+        "@context": CONTEXT,
+        "@type": EDC_PREFIX + "ContractDefinition",
+        "@id": "1",
+        EDC_PREFIX + "accessPolicyId": access_policy_id,
+        EDC_PREFIX + "contractPolicyId": contract_policy_id,
+        EDC_PREFIX + "assetsSelector": []
+    }
+
+    response = requests.post(connector_management_url + "v2/contractdefinitions",
+                             headers={'Content-Type': 'application/json'},
+                             data=json.dumps(contract_definition_data))
+    if verbose:
+        ic(response.status_code, json.loads(response.text))
+
+
+def query_catalog(provider_url, connector_management_url, verbose=True):
+    catalog_request_data = {
+        "@context": CONTEXT,
+        EDC_PREFIX + "providerUrl": provider_url,
+        EDC_PREFIX + "protocol": "dataspace-protocol-http"
+    }
+
+    response = requests.post(connector_management_url + "v2/catalog/request",
+                             headers={'Content-Type': 'application/json'},
+                             data=json.dumps(catalog_request_data))
+    if verbose:
+        ic(response.status_code, json.loads(response.text))
+
+    return json.loads(response.text)["dcat:dataset"]
+
+
+def negotiate_offer(connector_id, consumer_id, provider_id, connector_address, offer_id, asset_id, policy,
+                    connector_management_url, verbose=True):
+    consumer_offer_data = {
+        "@context": CONTEXT,
+        "@type": EDC_PREFIX + "NegotiationInitiateRequestDto",
+        EDC_PREFIX + "connectorId": connector_id,
+        EDC_PREFIX + "consumerId": consumer_id,
+        EDC_PREFIX + "providerId": provider_id,
+        EDC_PREFIX + "connectorAddress": connector_address,
+        EDC_PREFIX + "protocol": "dataspace-protocol-http",
+        EDC_PREFIX + "offer": {
+            "@type": EDC_PREFIX + "ContractOfferDescription",
+            EDC_PREFIX + "offerId": offer_id,
+            EDC_PREFIX + "assetId": asset_id,
+            EDC_PREFIX + "policy": policy
+        }
+    }
+
+    response = requests.post(connector_management_url + "v2/contractnegotiations",
+                             headers={'Content-Type': 'application/json'},
+                             data=json.dumps(consumer_offer_data))
+    if verbose:
+        ic(response.status_code, json.loads(response.text))
+
+    # extract negotiation id
+    return json.loads(response.text)["@id"]
+
+
+def poll_negotiation_until_finalized(connector_management_url, negotiation_id, verbose=True):
+    state = ""
+
+    while state != "FINALIZED":
+        ic("Requesting status of negotiation")
+        response = requests.get(connector_management_url + "v2/contractnegotiations/" + negotiation_id,
+                                headers={'Content-Type': 'application/json'})
+        state = json.loads(response.text)[EDC_PREFIX + "state"]
+        if verbose:
+            ic(state)
+        time.sleep(1)
+    if verbose:
+        ic(response.status_code, json.loads(response.text))
+    return json.loads(response.text)[EDC_PREFIX + "contractAgreementId"]
+
+
+def initiate_data_transfer(connector_id, connector_address, agreement_id, asset_id,
+                           connector_management_url, verbose=True):
+    transfer_data = {
+        "@context": CONTEXT,
+        "@type": EDC_PREFIX + "TransferRequestDto",
+        EDC_PREFIX + "connectorId": connector_id,
+        EDC_PREFIX + "connectorAddress": connector_address,
+        EDC_PREFIX + "contractId": agreement_id,
+        EDC_PREFIX + "assetId": asset_id,
+        EDC_PREFIX + "managedResources": False,
+        EDC_PREFIX + "protocol": "dataspace-protocol-http",
+        EDC_PREFIX + "dataDestination": {
+            "@type": EDC_PREFIX + "DataAddress",
+            EDC_PREFIX + "properties": {
+                EDC_PREFIX + "type": "HttpProxy"
+            }
+        }
+    }
+    response = requests.post(connector_management_url + "v2/transferprocesses",
+                             headers={'Content-Type': 'application/json'},
+                             data=json.dumps(transfer_data))
+    if verbose:
+        ic(response.status_code, json.loads(response.text))
+    return json.loads(response.text)["@id"]
+
+
+def poll_transfer_until_completed(connector_management_url, transfer_id, verbose=True):
+    state = ""
+
+    while state != "COMPLETED":
+        ic("Requesting status of transfer")
+        response = requests.get(connector_management_url + "v2/transferprocesses/" + transfer_id,
+                                headers={'Content-Type': 'application/json'})
+        state = json.loads(response.text)[EDC_PREFIX + "state"]
+        if verbose:
+            ic(state)
+        time.sleep(1)
+
+    if verbose:
+        ic(response.status_code, json.loads(response.text))
--- http-pull-v2-dsp.py
+++ http-pull-v2-dsp.py
@@ -3,6 +3,8 @@ import time
 import requests
 from icecream import ic
 import json
+from common_v2 import create_dataplane, create_asset, create_policy, create_contract_definition, query_catalog, \
+    negotiate_offer, poll_negotiation_until_finalized, initiate_data_transfer, poll_transfer_until_completed
 
 """
 Endpoint configuration
@@ -36,37 +38,13 @@ Connector initialization
 
 # Provider
 ic("Preparing provider connector dataplane")
-provider_dp_instance_data = {
-    "edctype": "dataspaceconnector:dataplaneinstance",
-    "id": "http-pull-provider-dataplane",
-    "url": provider_connector_control_url + "transfer",
-    "allowedSourceTypes": ["HttpData"],
-    "allowedDestTypes": ["HttpProxy", "HttpData"],
-    "properties": {
-        "publicApiUrl": provider_connector_public_url
-    }
-}
-response = requests.post(provider_connector_management_url + "instances",
-                         headers={'Content-Type': 'application/json'},
-                         data=json.dumps(provider_dp_instance_data))
-ic(response.status_code, response.text)
+create_dataplane(provider_connector_control_url + "transfer", provider_connector_public_url,
+                 provider_connector_management_url)
 
 # Consumer
 ic("Preparing consumer connector dataplane")
-consumer_dp_instance_data = {
-    "edctype": "dataspaceconnector:dataplaneinstance",
-    "id": "http-pull-consumer-dataplane",
-    "url": consumer_connector_control_url + "transfer",
-    "allowedSourceTypes": ["HttpData"],
-    "allowedDestTypes": ["HttpProxy", "HttpData"],
-    "properties": {
-        "publicApiUrl": consumer_connector_public_url
-    }
-}
-response = requests.post(consumer_connector_management_url + "instances",
-                         headers={'Content-Type': 'application/json'},
-                         data=json.dumps(consumer_dp_instance_data))
-ic(response.status_code, response.text)
+create_dataplane(consumer_connector_control_url + "transfer", consumer_connector_public_url,
+                 consumer_connector_management_url)
 
 """
 Create Asset
@@ -74,34 +52,9 @@ Create Asset
 
 # Provider
 ic("Creating asset in provider connector")
-asset_data = {
-    "@context": CONTEXT,
-    EDC_PREFIX + "asset": {
-        "@type": EDC_PREFIX + "Asset",
-        "@id": "assetId",
-        EDC_PREFIX + "properties": {
-            EDC_PREFIX + "name": "assetId",
-            EDC_PREFIX + "description": "product description",
-            EDC_PREFIX + "version": "0.4.2",
-            EDC_PREFIX + "contenttype": "application/json"
-        }
-    },
-    EDC_PREFIX + "dataAddress": {
-        "@type": EDC_PREFIX + "DataAddress",
-        EDC_PREFIX + "properties": {
-            EDC_PREFIX + "name": "Test asset",
-            EDC_PREFIX + "baseUrl": "https://jsonplaceholder.typicode.com/users",
-            EDC_PREFIX + "type": "HttpData"
-        }
-    }
-}
-
-response = requests.post(provider_connector_management_url + "v2/assets",
-                         headers={'Content-Type': 'application/json'},
-                         data=json.dumps(asset_data))
-ic(response.status_code, json.loads(response.text))
-# extract asset id from response
-asset_id = json.loads(response.text)["@id"]
+asset_id = create_asset("assetId", "My Asset", "Description", "v1.2.3", "application/json",
+                        "My Asset", "https://jsonplaceholder.typicode.com/users", "HttpData",
+                        provider_connector_management_url)
 
 """
 Create Policy
@@ -109,29 +62,7 @@ Create Policy
 
 # Provider
 ic("Creating policy in provider connector")
-policy_data = {
-    "@context": CONTEXT,
-    "@id": "aPolicy",
-    EDC_PREFIX + "policy": {
-        "@context": "http://www.w3.org/ns/odrl.jsonld",
-        ODRL_PREFIX + "permission": [
-            {
-                ODRL_PREFIX + "target": "assetId",
-                ODRL_PREFIX + "action": {
-                    ODRL_PREFIX + "type": "USE"
-                },
-                ODRL_PREFIX + "edctype": "dataspaceconnector:permission"
-            }
-        ],
-        "@type": ODRL_PREFIX + "Set"
-    }
-}
-
-response = requests.post(provider_connector_management_url + "v2/policydefinitions",
-                         headers={'Content-Type': 'application/json'},
-                         data=json.dumps(policy_data))
-ic(response.status_code, json.loads(response.text))
-policy_id = json.loads(response.text)["@id"]
+policy_id = create_policy("aPolicy", "assetId", provider_connector_management_url)
 
 """
 Create contract definition
@@ -139,19 +70,7 @@ Create contract definition
 
 # Provider
 ic("Create contract definition for the created asset and policy on provider connector")
-contract_definition_data = {
-    "@context": CONTEXT,
-    "@type": EDC_PREFIX + "ContractDefinition",
-    "@id": "1",
-    EDC_PREFIX + "accessPolicyId": policy_id,
-    EDC_PREFIX + "contractPolicyId": policy_id,
-    EDC_PREFIX + "assetsSelector": []
-}
-
-response = requests.post(provider_connector_management_url + "v2/contractdefinitions",
-                         headers={'Content-Type': 'application/json'},
-                         data=json.dumps(contract_definition_data))
-ic(response.status_code, json.loads(response.text))
+create_contract_definition(policy_id, policy_id, provider_connector_management_url)
 
 """
 Fetch catalog from provider
@@ -159,18 +78,8 @@ Fetch catalog from provider
 
 # Consumer asks own connector to query providers catalog
 ic("Query providers catalog")
-catalog_request_data = {
-    "@context": CONTEXT,
-    EDC_PREFIX + "providerUrl": provider_connector_dsp_url,
-    EDC_PREFIX + "protocol": "dataspace-protocol-http"
-}
 
-response = requests.post(consumer_connector_management_url + "v2/catalog/request",
-                         headers={'Content-Type': 'application/json'},
-                         data=json.dumps(catalog_request_data))
-ic(response.status_code, json.loads(response.text))
-
-offering_data = json.loads(response.text)["dcat:dataset"]
+offering_data = query_catalog(provider_connector_dsp_url, consumer_connector_management_url)
 if not isinstance(offering_data, dict):  # if there are multiple entries, choose the first one
     offering_data = offering_data[0]
 
@@ -184,90 +93,32 @@ Negotiate contract from available offerings
 # this fails immediately in ContractValidationServiceImpl.java:validateConfirmed(...) which checks for equality.
 
 ic("Negotiate offer")
-consumer_offer_data = {
-    "@context": CONTEXT,
-    "@type": EDC_PREFIX + "NegotiationInitiateRequestDto",
-    EDC_PREFIX + "connectorId": "provider",
-    EDC_PREFIX + "consumerId": "consumer",
-    EDC_PREFIX + "providerId": "provider",
-    EDC_PREFIX + "connectorAddress": provider_connector_dsp_url,
-    EDC_PREFIX + "protocol": "dataspace-protocol-http",
-    EDC_PREFIX + "offer": {
-        "@type": EDC_PREFIX + "ContractOfferDescription",
-        EDC_PREFIX + "offerId": offering_data["odrl:hasPolicy"]["@id"],
-        EDC_PREFIX + "assetId": offering_data["edc:id"],
-        EDC_PREFIX + "policy": offering_data["odrl:hasPolicy"]
-    }
-}
 
-response = requests.post(consumer_connector_management_url + "v2/contractnegotiations",
-                         headers={'Content-Type': 'application/json'},
-                         data=json.dumps(consumer_offer_data))
-ic(response.status_code, json.loads(response.text))
-
-# extract negotiation id
-negotiation_id = json.loads(response.text)["@id"]
+negotiation_id = negotiate_offer("provider", "consumer", "provider", provider_connector_dsp_url,
+                                 offering_data["odrl:hasPolicy"]["@id"], offering_data["edc:id"],
+                                 offering_data["odrl:hasPolicy"], consumer_connector_management_url)
 
 """
 Check negotiation status
 """
 ic("Check negotiation status and wait until finalized")
 # Consumer asks own connector for status
-state = ""
-
-while state != "FINALIZED":
-    ic("Requesting status of negotiation")
-    response = requests.get(consumer_connector_management_url + "v2/contractnegotiations/" + negotiation_id,
-                            headers={'Content-Type': 'application/json'})
-    state = json.loads(response.text)[EDC_PREFIX + "state"]
-    ic(state)
-    time.sleep(1)
-ic(response.status_code, json.loads(response.text))
-agreement_id = json.loads(response.text)[EDC_PREFIX + "contractAgreementId"]
+agreement_id = poll_negotiation_until_finalized(consumer_connector_management_url, negotiation_id)
 
 """
 Start data transfer
 """
 # Consumer asks own connector to start transfer
 ic("Initiate data transfer")
-transfer_data = {
-    "@context": CONTEXT,
-    "@type": EDC_PREFIX + "TransferRequestDto",
-    EDC_PREFIX + "connectorId": "provider",
-    EDC_PREFIX + "connectorAddress": provider_connector_dsp_url,
-    EDC_PREFIX + "contractId": agreement_id,
-    EDC_PREFIX + "assetId": asset_id,
-    EDC_PREFIX + "managedResources": False,
-    EDC_PREFIX + "protocol": "dataspace-protocol-http",
-    EDC_PREFIX + "dataDestination": {
-        "@type": EDC_PREFIX + "DataAddress",
-        EDC_PREFIX + "properties": {
-            EDC_PREFIX + "type": "HttpProxy"
-        }
-    }
-}
-response = requests.post(consumer_connector_management_url + "v2/transferprocesses",
-                         headers={'Content-Type': 'application/json'},
-                         data=json.dumps(transfer_data))
-ic(response.status_code, json.loads(response.text))
-transfer_id = json.loads(response.text)["@id"]
+transfer_id = initiate_data_transfer("provider", provider_connector_dsp_url, agreement_id, asset_id,
+                                     consumer_connector_management_url)
 
 """
 Check transfer status
 """
 # Consumer asks own connector for status
 ic("Check transfer status and wait until completed")
-state = ""
-
-while state != "COMPLETED":
-    ic("Requesting status of transfer")
-    response = requests.get(consumer_connector_management_url + "v2/transferprocesses/" + transfer_id,
-                            headers={'Content-Type': 'application/json'})
-    state = json.loads(response.text)[EDC_PREFIX + "state"]
-    ic(state)
-    time.sleep(1)
-
-ic(response.status_code, json.loads(response.text))
+poll_transfer_until_completed(consumer_connector_management_url, transfer_id)
 
 # at this point we ask the consumer backend service (separate from the connector) for the authentication token
 # and query the public endpoint of the provider with this authorization like a proxy
